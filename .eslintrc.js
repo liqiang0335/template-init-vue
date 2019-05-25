16b7f0c901e83c2c0c855e27fd894679ceedb0dc
liqiang <liqiang0335@qq.com>
2019-05-25 17:11:05 +0800

    add

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,17 +1,23 @@
 module.exports = {
-  extends: ["eslint:recommended", "plugin:vue/essential"],
+  extends: [
+    "eslint:recommended",
+    "plugin:react/recommended",
+    "plugin:vue/essential"
+  ],
   env: {
     browser: true,
     commonjs: true,
     es6: true
   },
   globals: {
-    importVueComps: true,
+    importName: true,
+    __dirname: true,
     process: true,
     path: true
   },
+  parser: "babel-eslint",
   parserOptions: {
-    ecmaVersion: 2017,
+    ecmaVersion: 2018,
     sourceType: "module",
     ecmaFeatures: {
       jsx: true
@@ -19,6 +25,10 @@ module.exports = {
   },
   rules: {
     semi: 2,
+    "react/react-in-jsx-scope": "off",
+    "react/prop-types": "off",
+    "react/display-name": "off",
+    "no-irregular-whitespace": "off",
     "no-console": "off"
   }
 };
